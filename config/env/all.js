2fc0a642bc75a9db090edda491112fca470a536b
troyth <troyth@gmail.com>
2014-05-26 14:30:42 -0400

    configs

--- config/env/all.js
+++ config/env/all.js
@@ -5,13 +5,13 @@ var rootPath = path.normalize(__dirname + '/../..');
 
 module.exports = {
 	root: rootPath,
-	port: process.env.PORT || 3000,
+	port: process.env.PORT || 2701,
 	db: process.env.MONGOHQ_URL,
 	templateEngine: 'swig',
 
 	// The secret should be set to a non-guessable string that
 	// is used to compute a session hash
-	sessionSecret: 'xgramsec',
+	sessionSecret: 'offushhh',
 	// The name of the MongoDB collection to store sessions in
 	sessionCollection: 'sessions'
 };
